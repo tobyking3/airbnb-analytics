1351c9325f10d5a1093d7f96c67db58d2ef71977
Toby King <toby.king@bbc.co.uk>
2019-01-25 14:01:47 +0000

    dev env (working)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
+import './styles.css';
 
 /*This can be especially helpful when implementing some sort of data visualization using a tool like d3.
 Instead of making an ajax request and parsing the data at runtime you can load it into your module during
@@ -9,19 +8,26 @@ the build process so that the parsed data is ready to go as soon as the module h
 
 function component() {
   let element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Airbnb', 'Analytics'], ' ');
-  element.classList.add('hello');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log("Working");
+  element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
