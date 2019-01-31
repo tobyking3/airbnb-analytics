663d0b00b1591d2c5808afe8910c3c096069af84
Toby King <toby.king@bbc.co.uk>
2019-01-31 11:34:13 +0000

    basic bar, line and scatter

--- src/index.js
+++ src/index.js
@@ -36,15 +36,19 @@ if (module.hot) {
   })
 }
 
-//====================================================================================================
-
+//========================================================
 const w = 300;
-const h = 100;
+const h = 120;
 const padding = 2;
-const dataset = [5, 10, 15, 20, 25];
+const dataset = [5, 10, 15, 20, 25, 5, 20, 13, 10, 22];
 const svg = d3.select(".barChart").append("svg")
               .attr("width", w)
               .attr("height", h)
+
+function colorPicker(v){
+  if (v<=20) { return "#666666";}
+  else if (v>20){ return "#FF0033";}
+}
 
 svg.selectAll("rect")
     .data(dataset)
@@ -55,5 +59,130 @@ svg.selectAll("rect")
         y: function(d) {return h - (d*4);},
         width: w / dataset.length - padding,
         height: function(d) {return d*4;},
-        fill: function(d) {return "rgb(0, " + (d*10) + ", 0)";}
+        fill: function(d) {return colorPicker(d);}
       });
+
+  svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function (d) {return d;})
+    .attrs({
+      "text-anchor": "middle",
+      x: function(d, i){return i * (w / dataset.length) + (w / dataset.length - padding) / 2;},
+      y: function (d) {return h - (d*4) + 14;},
+      "font-family": "sans-serif",
+      "font-size": 12,
+      "fill": "#ffffff"
+    });
+
+//When key has dash in it, it needs to be wrapped in speech marks//
+
+//=======================================================================
+
+const w2 = 350;
+const h2 = 400;
+
+const monthlySales = [
+    {"month":10, "sales":100},
+    {"month":20, "sales":130},
+    {"month":30, "sales":250},
+    {"month":40, "sales":300},
+    {"month":50, "sales":265},
+    {"month":60, "sales":225},
+    {"month":70, "sales":180},
+    {"month":80, "sales":120},
+    {"month":90, "sales":145},
+    {"month":100, "sales":130}
+  ];
+
+console.log(monthlySales);
+
+var lineFun = d3.line()
+    .x(function(d) {return d.month*3;})
+    .y(function(d) {return h2-d.sales;})
+    .curve(d3.curveLinear);
+    //.curve() defaults to linear if not declared
+
+var svg2 = d3.select(".lineChart").append("svg")
+              .attr("width", w2)
+              .attr("height", h2)
+
+var viz = svg2.append("path")
+  .attrs({
+    d: lineFun(monthlySales),
+    "stroke": "purple",
+    "stroke-width": 2,
+    "fill": "none"
+  })
+
+var labels = svg2.selectAll("text")
+    .data(monthlySales)
+    .enter()
+    .append("text")
+    .text(function (d) {return d.sales;})
+    .attrs({
+      x: function(d){return d.month*3 - 25;},
+      y: function (d){return h2-d.sales;},
+      "text-anchor": "start",
+      "font-family": "sans-serif",
+      "font-size": 12,
+      "fill": "orange",
+      "dy": ".35em",
+      "font-weight": function(d,i) {
+        if (i == 0 || i == (monthlySales.length-1)){return "bold"} 
+        else {return "normal"}
+      }
+    });
+
+//========================================================================
+
+//KPI
+
+function salesKPI(d){
+  if (d>=250) { return "#33CC66" }
+  else if (d<250) { return "#666666"}
+}
+
+function showMinMax(ds, col, val, type){
+  var max = d3.max(ds, function(d){ return d[col]; });
+  var min = d3.min(ds, function(d){ return d[col]; });
+
+  if (type == 'minmax' && (val == max || val == min)) {
+    return val;
+  } else {
+    if (type == 'all') {
+      return val;
+    }
+  }
+}
+
+var svg3 = d3.select(".scatterChart").append("svg")
+              .attr("width", w2)
+              .attr("height", h2)
+
+var labels = svg3.selectAll("circle")
+    .data(monthlySales)
+    .enter()
+    .append("circle")
+    .attrs({
+      cx: function(d){return d.month*3},
+      cy: function (d){return h2-d.sales;},
+      r: 5,
+      "fill": function(d) {return salesKPI(d.sales)}
+    });
+
+var labels = svg3.selectAll("text")
+    .data(monthlySales)
+    .enter()
+    .append("text")
+    .text(function (d) {return showMinMax(monthlySales, 'sales', d.sales, 'minmax');})
+    .attrs({
+      x: function(d){return d.month*3 - 30;},
+      y: function (d){return h2-d.sales;},
+      "text-anchor": "start",
+      "font-family": "sans-serif",
+      "font-size": 12,
+      "fill": "orange",
+      "dy": ".35em"
+    });
