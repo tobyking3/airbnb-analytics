08655787c087041cce435c07cb42f74f6d4fd0c9
Toby King <toby.king@bbc.co.uk>
2019-02-02 20:38:24 +0000

    geojson map

--- src/index.js
+++ src/index.js
@@ -35,14 +35,10 @@ if (module.hot) {
 }
 
 
-
-
-
-
 //======================JSON DATA=============================================================
 
-const h = 100;
-const w = 400;
+const h = 250;
+const w = 700;
 const padding = 20;
 
 function getDate(d){
@@ -342,6 +338,48 @@ d3.json("https://api.github.com/repos/tobyking3/airbnb-analytics/contents/src/da
 //     d3.select("#tooltip").remove();
 //   })
 
+//==========================================MAP============================================
 
-
+var wMap = 1400;
+var hMap = 700;
+var svgMap = d3.select("div#container").append("svg").attr("preserveAspectRatio", "xMinYMin meet").style("background-color","#c9e8fd")
+.attr("viewBox", "0 0 " + wMap + " " + hMap)
+.classed("svg-content", true);
+var projection = d3.geoMercator().translate([wMap/2, hMap/2]).scale(2200).center([0,40]);
+var path = d3.geoPath().projection(projection);
+        
+  // load data  
+var worldmap = d3.json("countries.geojson");
+var cities = d3.csv("cities.csv");
+   
+Promise.all([worldmap, cities]).then(function(values){    
+ // draw map
+    svgMap.selectAll("path")
+        .data(values[0].features)
+        .enter()
+        .append("path")
+        .attr("class","continent")
+        .attr("d", path),
+ // draw points
+    svgMap.selectAll("circle")
+        .data(values[1])
+        .enter()
+        .append("circle")
+    .attr("class","circles")
+        .attr("cx", function(d) {return projection([d.Longitude, d.Lattitude])[0];})
+        .attr("cy", function(d) {return projection([d.Longitude, d.Lattitude])[1];})
+        .attr("r", "1px"),
+ // add labels
+    svgMap.selectAll("text")
+        .data(values[1])
+        .enter()
+        .append("text")
+    .text(function(d) {
+            return d.City;
+         })
+    .attr("x", function(d) {return projection([d.Longitude, d.Lattitude])[0] + 5;})
+    .attr("y", function(d) {return projection([d.Longitude, d.Lattitude])[1] + 15;})
+    .attr("class","labels");
+        
+    }); 
 
