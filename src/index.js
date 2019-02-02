5925e01193d845acf7722ec5f455350aa803bf7d
Toby King <toby.king@bbc.co.uk>
2019-02-02 17:12:38 +0000

    line chart tooltip

--- src/index.js
+++ src/index.js
@@ -41,156 +41,189 @@ if (module.hot) {
 
 //======================JSON DATA=============================================================
 
-// const h = 100;
-// const w = 400;
-// const padding = 20;
+const h = 100;
+const w = 400;
+const padding = 20;
 
-// function getDate(d){
-//   var strDate = new String(d);
-//   var year = strDate.substr(0, 4);
-//   var month = strDate.substr(4, 2)-1; //zero based index
-//   var day = strDate.substr(6, 2);
+function getDate(d){
+  var strDate = new String(d);
+  var year = strDate.substr(0, 4);
+  var month = strDate.substr(4, 2)-1; //zero based index
+  var day = strDate.substr(6, 2);
 
-//   return new Date(year, month, day);
-// }
+  return new Date(year, month, day);
+}
 
-// function updateLine(ds){
+function updateLine(ds){
 
-//   var yMin = 0;
-//   var yMax = d3.max(ds.monthlySales, function(d){return d.sales;});
+  var yMin = 0;
+  var yMax = d3.max(ds.monthlySales, function(d){return d.sales;});
 
-//   var minDate = getDate(ds.monthlySales[0]['month']);
-//   var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
+  var minDate = getDate(ds.monthlySales[0]['month']);
+  var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
 
-//   //==============================================================
+  //==============================================================
 
-//   var xScale = d3.scaleTime()
-//     .domain([minDate, maxDate])
-//     .range([padding, w - padding]);
+  var xScale = d3.scaleTime()
+    .domain([minDate, maxDate])
+    .range([padding, w - padding]);
 
-//   var yScale = d3.scaleLinear()
-//     .domain([yMin, yMax])
-//     .range([h-padding, 0]);
+  var yScale = d3.scaleLinear()
+    .domain([yMin, yMax])
+    .range([h-padding, 0]);
 
-//   //==============================================================
+  //==============================================================
 
-//   var yAxisGen = d3.axisLeft(yScale)
-//                     .ticks(4);
+  var yAxisGen = d3.axisLeft(yScale)
+                    .ticks(4);
 
-//   var xAxisGen = d3.axisBottom(xScale)
-//                     .tickFormat(d3.timeFormat("%b"))
-//                     .ticks(ds.monthlySales.length-1);
+  var xAxisGen = d3.axisBottom(xScale)
+                    .tickFormat(d3.timeFormat("%b"))
+                    .ticks(ds.monthlySales.length-1);
 
-//   //==============================================================
+  //==============================================================
 
-//   var externalLine = d3.line()
-//     .x(function(d) {return xScale(getDate(d.month));})
-//     .y(function(d) {return yScale(d.sales);})
+  var externalLine = d3.line()
+    .x(function(d) {return xScale(getDate(d.month));})
+    .y(function(d) {return yScale(d.sales);})
 
-//   var svg = d3.select(".JSONChart").select("#svg-" + ds.category);
+  var svg = d3.select(".JSONChart").select("#svg-" + ds.category);
 
-//   var yAxis = svg.selectAll("g.y-axis").call(yAxisGen);
+  var yAxis = svg.selectAll("g.y-axis").call(yAxisGen);
 
-//   var xAxis = svg.selectAll("g.x-axis").call(xAxisGen);
+  var xAxis = svg.selectAll("g.x-axis").call(xAxisGen);
 
-//   var viz = svg.selectAll(".path-" + ds.category)
-//   .transition()
-//   .duration(2000)
-//   .ease(d3.easeBounce)
-//   .attrs({
-//     d: externalLine(ds.monthlySales),
-//   })
-// }
+  var viz = svg.selectAll(".path-" + ds.category)
+  .transition()
+  .duration(2000)
+  .ease(d3.easeBounce)
+  .attrs({
+    d: externalLine(ds.monthlySales),
+  })
+}
 
-// function buildLine(ds){
+function buildLine(ds){
 
-//   var xMin = d3.min(ds.monthlySales, function(d){return d.month;});
-//   var xMax = d3.max(ds.monthlySales, function(d){return d.month;});
-//   var yMin = 0;
-//   var yMax = d3.max(ds.monthlySales, function(d){return d.sales;});
+  var xMin = d3.min(ds.monthlySales, function(d){return d.month;});
+  var xMax = d3.max(ds.monthlySales, function(d){return d.month;});
+  var yMin = 0;
+  var yMax = d3.max(ds.monthlySales, function(d){return d.sales;});
 
-//   var minDate = getDate(ds.monthlySales[0]['month']);
-//   var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
+  var minDate = getDate(ds.monthlySales[0]['month']);
+  var maxDate = getDate(ds.monthlySales[ds.monthlySales.length - 1]['month']);
 
-//   //==============================================================
+  //==============================================================
 
-//   var xScale = d3.scaleTime()
-//     .domain([minDate, maxDate])
-//     .range([padding, w - padding]);
+  var tooltip = d3.select(".JSONChart").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-//   var yScale = d3.scaleLinear()
-//     .domain([yMin, yMax])
-//     .range([h-padding, 0]);
+  //==============================================================
 
-//   //==============================================================
+  var xScale = d3.scaleTime()
+    .domain([minDate, maxDate])
+    .range([padding, w - padding]);
 
-//   var yAxisGen = d3.axisLeft(yScale).ticks(4);
-//   var xAxisGen = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b"));
+  var yScale = d3.scaleLinear()
+    .domain([yMin, yMax])
+    .range([h-padding, 0]);
 
-//   //==============================================================
+  //==============================================================
 
-//   var externalLine = d3.line()
-//     .x(function(d) {return xScale(getDate(d.month));})
-//     .y(function(d) {return yScale(d.sales);})
+  var yAxisGen = d3.axisLeft(yScale).ticks(4);
+  var xAxisGen = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b"));
 
-//   var svg = d3.select(".JSONChart").append("svg")
-//     .attr("width", w)
-//     .attr("height", h)
-//     .attr("id", "svg-" + ds.category);
+  //==============================================================
 
-//   var yAxis = svg.append("g").call(yAxisGen)
-//     .attrs({
-//       "class": "y-axis",
-//       "transform": "translate(" + padding + ", 0)"
-//     })
+  var externalLine = d3.line()
+    .x(function(d) {return xScale(getDate(d.month));})
+    .y(function(d) {return yScale(d.sales);})
 
-//   var xAxis = svg.append("g").call(xAxisGen)
-//     .attrs({
-//       "class": "x-axis",
-//       "transform": "translate(0, " + (h - padding) + ")"
-//     })
+  var svg = d3.select(".JSONChart").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("id", "svg-" + ds.category);
 
-//   var viz = svg.append("path")
-//   .attrs({
-//     d: externalLine(ds.monthlySales),
-//     "stroke": "purple",
-//     "stroke-width": 2,
-//     "fill": "none",
-//     "class": "path-" + ds.category
-//   })
-// }
+  var yAxis = svg.append("g").call(yAxisGen)
+    .attrs({
+      "class": "y-axis",
+      "transform": "translate(" + padding + ", 0)"
+    })
 
-// //===============================================================
+  var xAxis = svg.append("g").call(xAxisGen)
+    .attrs({
+      "class": "x-axis",
+      "transform": "translate(0, " + (h - padding) + ")"
+    })
 
-// function showHeader(ds){
-//   d3.select(".JSONChart").append("h2")
-//     .text(ds.category + "Sales (2013)");
-// }
+  var viz = svg.append("path")
+  .attrs({
+    d: externalLine(ds.monthlySales),
+    "stroke": "purple",
+    "stroke-width": 2,
+    "fill": "none",
+    "class": "path-" + ds.category
+  })
 
-// //===============================================================
+  var dots = svg.selectAll("circle")
+    .data(ds.monthlySales)
+    .enter()
+    .append("circle")
+    .attrs({
+      cx: function(d) {return xScale(getDate(d.month));},
+      cy: function(d) {return yScale(d.sales);},
+      r: 3,
+      "fill": "blue",
+      "class": "circle-" + ds.category
+    })
+    .on("mouseover", function(d){
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0.85);
 
-// d3.json("https://api.github.com/repos/tobyking3/airbnb-analytics/contents/src/data/MonthlySalesbyCategoryMultiple.json").then(function(data) {
-//   var decodedData = JSON.parse(window.atob(data.content));
-//   decodedData.contents.forEach(function(ds){
-//     showHeader(ds);
-//     buildLine(ds);
-//   })
+      tooltip.html("<strong>Sales $" + d.sales + "K</strong>")
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px")
+    })
+    .on("mouseout", function(d){
+      tooltip.transition()
+        .duration(300)
+        .style("opacity", 0);
+    })
 
-//   //add event listener
-//   d3.select("select")
-//     .on("change", function(d, i){
-//       var sel=d3.select("#date-option").node().value;
+}
 
-//       var decodedData = JSON.parse(window.atob(data.content));
+//===============================================================
+
+function showHeader(ds){
+  d3.select(".JSONChart").append("h2")
+    .text(ds.category + "Sales (2013)");
+}
+
+//===============================================================
+
+d3.json("https://api.github.com/repos/tobyking3/airbnb-analytics/contents/src/data/MonthlySalesbyCategoryMultiple.json").then(function(data) {
+  var decodedData = JSON.parse(window.atob(data.content));
+  decodedData.contents.forEach(function(ds){
+    showHeader(ds);
+    buildLine(ds);
+  })
+
+  //add event listener
+  d3.select("select")
+    .on("change", function(d, i){
+      var sel=d3.select("#date-option").node().value;
+
+      var decodedData = JSON.parse(window.atob(data.content));
       
-//       decodedData.contents.forEach(function(ds){
-//         //filter array based on selection
-//         ds.monthlySales.splice(0,ds.monthlySales.length-sel)
-//         //update line
-//         updateLine(ds);
-//       })
-//     })
-// })
+      decodedData.contents.forEach(function(ds){
+        //filter array based on selection
+        ds.monthlySales.splice(0,ds.monthlySales.length-sel)
+        //update line
+        updateLine(ds);
+      })
+    })
+})
 
 //========================================================================================================================
 
@@ -266,47 +299,48 @@ if (module.hot) {
 //       .text(function(d){ return showMinMax(monthlySales, 'sales', d.sales, sel);})
 //   })
 
+//==========================================BAR CHART TOOLTIP==============================================
 
-const w = 300;
-const h = 120;
-const padding = 2;
-const dataset = [5, 10, 15, 20, 25, 5, 20, 13, 10, 22];
-const svg = d3.select(".barChart").append("svg")
-              .attr("width", w)
-              .attr("height", h)
+// const w = 300;
+// const h = 120;
+// const padding = 2;
+// const dataset = [5, 10, 15, 20, 25, 5, 20, 13, 10, 22];
+// const svg = d3.select(".barChart").append("svg")
+//               .attr("width", w)
+//               .attr("height", h)
 
-function colorPicker(v){
-  if (v<=20) { return "#666666";}
-  else if (v>20){ return "#FF0033";}
-}
+// function colorPicker(v){
+//   if (v<=20) { return "#666666";}
+//   else if (v>20){ return "#FF0033";}
+// }
 
-svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-  .attrs({
-        x: function(d, i) {return i * (w / dataset.length);},
-        y: function(d) {return h - (d*4);},
-        width: w / dataset.length - padding,
-        height: function(d) {return d*4;},
-        fill: function(d) {return colorPicker(d);}
-      })
-  .on("mouseover", function(d){
-    svg.append("text")
-    .text(d)
-    .attrs({
-      "text-anchor": "middle",
-      x: parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width")/2),
-      y: parseFloat(d3.select(this).attr("y")) + 12,
-      "font-family": "sans-serif",
-      "font-size": 12,
-      "fill": "#ffffff",
-      "id": "tooltip"
-    });
-  })
-  .on("mouseout", function(d){
-    d3.select("#tooltip").remove();
-  })
+// svg.selectAll("rect")
+//     .data(dataset)
+//     .enter()
+//     .append("rect")
+//   .attrs({
+//         x: function(d, i) {return i * (w / dataset.length);},
+//         y: function(d) {return h - (d*4);},
+//         width: w / dataset.length - padding,
+//         height: function(d) {return d*4;},
+//         fill: function(d) {return colorPicker(d);}
+//       })
+//   .on("mouseover", function(d){
+//     svg.append("text")
+//     .text(d)
+//     .attrs({
+//       "text-anchor": "middle",
+//       x: parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width")/2),
+//       y: parseFloat(d3.select(this).attr("y")) + 12,
+//       "font-family": "sans-serif",
+//       "font-size": 12,
+//       "fill": "#ffffff",
+//       "id": "tooltip"
+//     });
+//   })
+//   .on("mouseout", function(d){
+//     d3.select("#tooltip").remove();
+//   })
 
 
 
