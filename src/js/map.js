64f70579a2aa1d608321aa7f2c917ef68dabd417
Toby King <toby.king@bbc.co.uk>
2019-03-14 14:16:50 +0000

    styled map with dynamic data

--- src/js/map.js
+++ src/js/map.js
@@ -5,9 +5,12 @@ var mWidth = 400;
 var mHeight = 250;
 var mScale = 20000;
 
-var ColorEntire = "#DC2B61"
-var ColorPrivate = "#FFBA01"
-var ColorShared = "#57DEE3"
+var ColorHighlighted = "#3E67FF";
+var ColorUnhighlighted = "#C6DAFB";
+
+var ColorEntire = "#DC2B61";
+var ColorPrivate = "#FFBA01";
+var ColorShared = "#57DEE3";
 
 var active = d3.select(null);
 
@@ -21,6 +24,49 @@ var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
 var zoom = d3.zoom().scaleExtent([1, 4]).on("zoom", zoomed);
+
+//==============================================================
+
+function highlightBorough(d) {
+  d3.select(this)
+  .attrs({
+    "fill": ColorHighlighted // add striped fill
+  });
+}
+
+function unhighlightBorough(d) {
+  d3.select(this)
+  .attrs({
+    "fill": ColorUnhighlighted // add striped fill
+  });
+}
+
+function clicked(d) {
+
+  d3.selectAll(".point-" + d.properties.neighbourhood.replace(/\s+/g, '-'))
+    .style("display", "block");
+
+  d3.select(".map-value-borough").text(d.properties.neighbourhood);
+
+  if (active.node() === this) return reset();
+
+  active.classed("active", false);
+  active = d3.select(this).classed("active", true);
+
+  var bounds = path.bounds(d),
+      dx = bounds[1][0] - bounds[0][0],
+      dy = bounds[1][1] - bounds[0][1],
+      x = (bounds[0][0] + bounds[1][0]) / 2,
+      y = (bounds[0][1] + bounds[1][1]) / 2,
+      scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / mWidth, dy / mHeight))),
+      translate = [mWidth / 4 - scale * 2, mHeight - 40 - scale * 2];
+
+  svgMap.transition()
+      .duration(1000)
+      .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) );
+}
+
+//==============================================================
 
 //Initialize Map
 var svgMap = d3.select(".map")
@@ -45,9 +91,11 @@ d3.csv("listings.csv").then(function(csv){
       .attrs({
         "class": "map_borough-path",
         "d": path,
-        "fill": "#a2e7ff"
+        "fill": ColorUnhighlighted,
       })
-      .on("click", clicked);
+      .on("click", clicked)
+      .on("mouseover", highlightBorough)
+      .on("mouseout", unhighlightBorough);
 
     svgMap.selectAll('.property-label')
       .data(csv)
@@ -60,9 +108,10 @@ d3.csv("listings.csv").then(function(csv){
               "cx": projection([parseFloat(d.longitude), parseFloat(d.latitude)])[0],
               "cy": projection([parseFloat(d.longitude), parseFloat(d.latitude)])[1],
               "fill": propertyTypeColor(d.room_type),
-              "class": "point-" + d.neighbourhood,
+              "class": "point-" + d.neighbourhood.replace(/\s+/g, '-')
             })
-            .style("opacity", 0)
+            .style("opacity", 0.5)
+            .style("display", "none")
             ;
         })        
         .on("mouseover", showPropertyDetails)
@@ -157,25 +206,6 @@ function hidePropertyDetails(){
     .style("opacity", 0);
 }
 
-function clicked(d) {
-  if (active.node() === this) return reset();
-
-  active.classed("active", false);
-  active = d3.select(this).classed("active", true);
-
-  var bounds = path.bounds(d),
-      dx = bounds[1][0] - bounds[0][0],
-      dy = bounds[1][1] - bounds[0][1],
-      x = (bounds[0][0] + bounds[1][0]) / 2,
-      y = (bounds[0][1] + bounds[1][1]) / 2,
-      scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / mWidth, dy / mHeight))),
-      translate = [mWidth / 4 - scale * 2, mHeight - 40 - scale * 2];
-
-  svgMap.transition()
-      .duration(1000)
-      .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) );
-}
-
 function zoomed() {
   svgMap.attr("transform", d3.event.transform);
 }
@@ -189,25 +219,51 @@ function reset() {
       .call( zoom.transform, d3.zoomIdentity );
 }
 
-function boroughStats(d, i){
-  d3.selectAll(".point-" + d.properties.neighbourhood)
-    .style("opacity", 0.5);
+// Print the Stats
 
+function boroughStats(d, i){
+  createPieChart(d, i);
 
   d3.select(".map-value-borough").text(d.properties.neighbourhood);
   d3.select(".map-value-properties").text(d.properties["stats"]["totalNumProperties"]);
 
-  createPieChart(d, i);
+  d3.select(".panel-average-value_entire").text("£" + Math.round(d.properties["stats"]["entireAveragePrice"]));
+  d3.select(".panel-properties-value_entire").text(d.properties["stats"]["entireNumProperties"]);
 
-  d3.select(".home-type-average").text("£" + Math.round(d.properties["stats"]["entireAveragePrice"]));
-  d3.select(".home-type-properties").text(d.properties["stats"]["entireNumProperties"]);
-  d3.select(".home-type-percentage").text((d.properties["stats"]["entirePercentage"] * 100).toFixed(2) + "%");
+  d3.select(".panel-average-value_private").text("£" + Math.round(d.properties["stats"]["privateAveragePrice"]));
+  d3.select(".panel-properties-value_private").text(d.properties["stats"]["privateNumProperties"]);
 
-  d3.select(".private-type-average").text("£" + Math.round(d.properties["stats"]["privateAveragePrice"]));
-  d3.select(".private-type-properties").text(d.properties["stats"]["privateNumProperties"]);
-  d3.select(".private-type-percentage").text((d.properties["stats"]["privatePercentage"] * 100).toFixed(2) + "%");
-
-  d3.select(".shared-type-average").text("£" + Math.round(d.properties["stats"]["sharedAveragePrice"]));
-  d3.select(".shared-type-properties").text(d.properties["stats"]["sharedNumProperties"]);
-  d3.select(".shared-type-percentage").text((d.properties["stats"]["sharedPercentage"] * 100).toFixed(2) + "%");
+  d3.select(".panel-average-value_shared").text("£" + Math.round(d.properties["stats"]["sharedAveragePrice"]));
+  d3.select(".panel-properties-value_shared").text(d.properties["stats"]["sharedNumProperties"]);
 }
+
+// Handle the display of stats
+
+d3.selectAll(".panel-average-value").style("display", "none");
+d3.selectAll(".panel-properties-value").style("display", "none");
+d3.selectAll(".panel-average-value_entire").style("display", "block");
+d3.selectAll(".panel-properties-value_entire").style("display", "block");
+
+d3.select(".panel-select-type select")
+.on("change", function(d, i){
+  var select = d3.select("#panel-select-type-property").node().value;
+  if(select === 'Entire home/apt'){
+    d3.selectAll(".panel-average-value").style("display", "none");
+    d3.selectAll(".panel-properties-value").style("display", "none");
+    d3.selectAll(".panel-average-value_entire").style("display", "block");
+    d3.selectAll(".panel-properties-value_entire").style("display", "block");
+  };
+  if(select === 'Private room'){
+    d3.selectAll(".panel-average-value").style("display", "none");
+    d3.selectAll(".panel-properties-value").style("display", "none");
+    d3.selectAll(".panel-average-value_private").style("display", "block");
+    d3.selectAll(".panel-properties-value_private").style("display", "block");
+  };
+  if(select === 'Shared room'){
+    d3.selectAll(".panel-average-value").style("display", "none");
+    d3.selectAll(".panel-properties-value").style("display", "none");
+    d3.selectAll(".panel-average-value_shared").style("display", "block");
+    d3.selectAll(".panel-properties-value_shared").style("display", "block");
+  };
+  
+})
