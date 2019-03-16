6e41a221e0c274e0072f48203c6bd4c9f31c3f42
Toby King <toby.king@bbc.co.uk>
2019-03-16 15:21:46 +0000

    chart with borough select

--- src/js/line-chart.js
+++ src/js/line-chart.js
@@ -1,13 +1,15 @@
 import * as d3 from 'd3';
 import calendar_data from '../data/calendar.json';
 
+//===========================Format Data=====================================================
+
 // var newArray = [];
 // var newObj = {};
 
 // for (var borough in calendar_data) {
 //   newObj[borough] = [];
 //   for (var date in calendar_data[borough]) {
-//     var formattedDate = date.replace(/-/g,"");
+//     var formattedDate = parseInt(date.replace(/-/g,"") , 10);
 //     newObj[borough].push(
 //       {
 //         "date":formattedDate,
@@ -19,36 +21,55 @@ import calendar_data from '../data/calendar.json';
 
 // console.log(JSON.stringify(newObj));
 
-//======================JSON DATA=============================================================
+//======================Create Borough List===================================================
 
-const h = 500;
+var newArray = [];
+var arrayCount = 0;
+
+for (var borough in calendar_data) {
+  newArray.push(
+      borough
+    );
+}
+
+//=========================Select==============================================================
+
+var selectBox = document.getElementById('calendar-borough-select');
+
+for(var i = 0, l = newArray.length; i < l; i++){
+  var borough = newArray[i];
+  selectBox.options.add( new Option(borough, borough) );
+}
+
+//======================CHART==================================================================
+
+const h = 320;
 const w = 900;
-const padding = 20;
+const padding = 40;
 
 d3.json("calendar-array.json").then(function(data) {
-  buildLine(data['Barking and Dagenham'])
-  // d3.select("select").on("change", function(d, i){
-  //   var sel=d3.select("#date-option").node().value;
-  //       data.splice(0,data.length-sel)
-  //       updateLine(data);
-  // })
+  buildLine(data['Barking and Dagenham']);
+
+  d3.select("#calendar-borough-select").on("change", function(d, i){
+    var sel = d3.select("#calendar-borough-select").node().value;
+    updateLine(data[sel]);
+  })
 })
 
 function getDate(d){
   var strDate = new String(d);
   var year = strDate.substr(0, 4);
-  var month = strDate.substr(5, 2)-1; //zero based index
-  var day = strDate.substr(8, 2);
+  var month = strDate.substr(4, 2)-1;
+  var day = strDate.substr(6, 2);
 
   return new Date(year, month, day);
 }
 
 function buildLine(ds){
-  console.log(ds);
   var xMin = d3.min(ds, function(d){return d.date;});
   var xMax = d3.max(ds, function(d){return d.date;});
 
-  var yMin = (d3.min(ds, function(d){return d.booked;}));
+  var yMin = d3.min(ds, function(d){return d.booked;});
   var yMax = d3.max(ds, function(d){return d.booked;});
 
   var minDate = getDate(ds[0]['date']);
@@ -66,7 +87,7 @@ function buildLine(ds){
 
   //==============================================================
 
-  var yAxisGen = d3.axisLeft(yScale).ticks(10);
+  var yAxisGen = d3.axisLeft(yScale).ticks(20);
   var xAxisGen = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b"));
 
   //==============================================================
@@ -76,18 +97,22 @@ function buildLine(ds){
     .y(function(d) {return yScale(d.booked);})
 
   var svg = d3.select(".line-chart").append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("id", "svg-bookings");
+    .attrs({
+      "preserveAspectRatio": "xMinYMin meet",
+      "viewBox": "0 0 " + w + " " + h,
+      "id": "svg-bookings"
+    });
 
   var yAxis = svg.append("g").call(yAxisGen)
     .attrs({
+      "font-family": "GilroyLight",
       "class": "y-axis",
       "transform": "translate(" + padding + ", 0)"
     })
 
   var xAxis = svg.append("g").call(xAxisGen)
     .attrs({
+      "font-family": "GilroyBold",
       "class": "x-axis",
       "transform": "translate(0, " + (h - padding) + ")"
     })
@@ -95,9 +120,6 @@ function buildLine(ds){
   var viz = svg.append("path")
   .attrs({
     d: externalLine(ds),
-    "stroke": "purple",
-    "stroke-width": 1,
-    "fill": "none",
     "class": "path-bookings"
   })
 }
@@ -106,7 +128,7 @@ function buildLine(ds){
 
 function updateLine(ds){
 
-  var yMin = (d3.min(ds, function(d){return d.booked;}) - 500 );
+  var yMin = d3.min(ds, function(d){return d.booked;});
   var yMax = d3.max(ds, function(d){return d.booked;});
 
   var minDate = getDate(ds[0]['date']);
@@ -124,12 +146,7 @@ function updateLine(ds){
 
   //==============================================================
 
-  var yAxisGen = d3.axisLeft(yScale)
-                    .ticks(4);
-
-  var xAxisGen = d3.axisBottom(xScale)
-                    .tickFormat(d3.timeFormat("%b"))
-                    .ticks(ds.length-1);
+  var yAxisGen = d3.axisLeft(yScale).ticks(20);
 
   //==============================================================
 
@@ -137,15 +154,13 @@ function updateLine(ds){
     .x(function(d) {return xScale(getDate(d.date));})
     .y(function(d) {return yScale(d.booked);})
 
-  var svg = d3.select(".lineChart").select("#svg-bookings");
+  var svg = d3.select(".line-chart").select("#svg-bookings");
 
   var yAxis = svg.selectAll("g.y-axis").call(yAxisGen);
 
-  var xAxis = svg.selectAll("g.x-axis").call(xAxisGen);
-
   var viz = svg.selectAll(".path-bookings")
   .transition()
-  .duration(2000)
+  .duration(1000)
   .ease(d3.easeBounce)
   .attrs({
     d: externalLine(ds),
