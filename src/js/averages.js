52f5f377d9038911b266d60b861956159362e1f4
Toby King <toby.king@bbc.co.uk>
2019-04-25 21:10:52 +0100

    unit tests

--- src/js/averages.js
+++ src/js/averages.js
@@ -1,133 +1,126 @@
 import mapData from '../data/map-before.json';
 import listingsData from '../data/listings.json'
 
-let Averages = {};
-let neighbourhoodList = [];
+let totalEntirePrice = 0;
+let totalPrivatePrice = 0;
+let totalSharedPrice = 0;
 
-Averages.createBoroughList = function(dMap) {
+let numberOfEntireProperties = 0;
+let numberOfPrivateProperties = 0;
+let numberOfSharedProperties = 0;
 
-  let boroughList = [];
+let entireAverage = 0;
+let privateAverage = 0;
+let sharedAverage = 0;
 
-  dMap.features.forEach(function(borough, boroughIndex){
-    boroughList.push(borough.properties.neighbourhood);
-  });
+class Polygon {
 
-  return boroughList;
+  constructor(dMap, dListings) {
+    let dBoroughList = this.createList(dMap);
+    let dSortedListings = this.sortListings(dListings, dBoroughList);
+    let dAggregatedListings = this.calculateAverages(dSortedListings);
+    let combineData = this.combineData(dMap, dAggregatedListings);
+  }
+
+  createList(dMap) {
+    let boroughList = [];
+
+    dMap.features.forEach(function(borough, boroughIndex){
+      boroughList.push(borough.properties.neighbourhood);
+    });
+
+    return boroughList;
+  }
+
+  sortListings(dListings, boroughArray) {
+
+    //*********HIGHLIGHTS ERROR - ARRAY INSTEAD OF OBJECT************
+
+    var sortedListings = {};
+
+    boroughArray.forEach(function(nItem, nIndex){
+
+      sortedListings[nItem] = [];
+
+      dListings.forEach(function(item, index){
+
+        if(item['neighbourhood'] === nItem){
+
+          sortedListings[nItem].push(item);
+
+        }
+
+      });
+    });
+
+    return sortedListings
+  }
+
+  calculateAverages(dListings){
+
+    let dAverages = {};
+
+    Object.keys(dListings).forEach(function(key) {
+
+      dListings[key].forEach(function(nItem, nIndex){
+        if(nItem["room_type"] === "Entire home/apt"){
+          numberOfEntireProperties++;
+          totalEntirePrice = totalEntirePrice + nItem["price"];
+        }
+        if(nItem["room_type"] === "Private room"){
+          numberOfPrivateProperties++;
+          totalPrivatePrice = totalPrivatePrice + nItem["price"];
+        }
+        if(nItem["room_type"] === "Shared room"){
+          numberOfSharedProperties++;
+          totalSharedPrice = totalSharedPrice + nItem["price"];
+        }
+      })
+
+      entireAverage = totalEntirePrice / numberOfEntireProperties;
+
+      privateAverage = totalPrivatePrice / numberOfPrivateProperties;
+
+      sharedAverage = totalSharedPrice / numberOfSharedProperties;
+
+      dAverages[key] = {
+        "totalNumProperties": dListings[key].length,
+        "entireAveragePrice": entireAverage,
+        "entireNumProperties": numberOfEntireProperties,
+        "entirePercentage": numberOfEntireProperties / dListings[key].length,
+        "privateAveragePrice": privateAverage,
+        "privateNumProperties": numberOfPrivateProperties,
+        "privatePercentage": numberOfPrivateProperties / dListings[key].length,
+        "sharedAveragePrice": sharedAverage,
+        "sharedNumProperties": numberOfSharedProperties,
+        "sharedPercentage": numberOfSharedProperties / dListings[key].length,
+        "propertiesComparison": [
+          {"propertyType":"Entire home/apt", "numberOfProperties":numberOfEntireProperties}, 
+          {"propertyType":"Private room", "numberOfProperties":numberOfPrivateProperties}, 
+          {"propertyType":"Shared room", "numberOfProperties":numberOfSharedProperties}
+        ]
+      };
+
+    });
+
+    return dAverages;
+
+  }
+
+  combineData(dMap, dAverages){
+
+    dMap["features"].forEach(function(featuresItem){
+
+      var neighbourhood = featuresItem["properties"]["neighbourhood"];
+
+      featuresItem["properties"]["stats"] = dAverages[neighbourhood];
+
+    })
+
+    return dMap;
+
+  }
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Create list of neighborhoods
-
-mapData.features.forEach(function(borough, boroughIndex){
-    neighbourhoodList.push(borough.properties.neighbourhood);
-});
-
-// console.log(JSON.stringify(neighbourhoodList));
-
-//Sort listings by borough
-var sortedListings = [];
-
-neighbourhoodList.forEach(function(nItem, nIndex){
-  //For each neighborhood add a borough key to the new array
-  sortedListings[nItem] = [];
-  //Push each item in the listing data to the array if the neighborhood matches the key 
-    listingsData.forEach(function(item, index){
-        if(item['neighbourhood'] === nItem){
-          sortedListings[nItem].push(item);
-        }
-    });
-});
-
-// console.log(sortedListings);
-
-
-
-var dAverages = {};
-
-Object.keys(sortedListings).forEach(function(key) {
-  var totalEntirePrice = 0;
-  var totalPrivatePrice = 0;
-  var totalSharedPrice = 0;
-
-  var numberOfEntireProperties = 0;
-  var numberOfPrivateProperties = 0;
-  var numberOfSharedProperties = 0;
-
-  var entireAverage = 0;
-  var privateAverage = 0;
-  var sharedAverage = 0;
-
-  var entireUpper = 0;
-  var privateUpper = 0;
-  var sharedUpper = 0;
-
-  var entireLower = 0;
-  var privateLower = 0;
-  var sharedLower = 0;
-
-  sortedListings[key].forEach(function(nItem, nIndex){
-    if(nItem["room_type"] === "Entire home/apt"){
-      numberOfEntireProperties++;
-      totalEntirePrice = totalEntirePrice + nItem["price"];
-    }
-    if(nItem["room_type"] === "Private room"){
-      numberOfPrivateProperties++;
-      totalPrivatePrice = totalPrivatePrice + nItem["price"];
-    }
-    if(nItem["room_type"] === "Shared room"){
-      numberOfSharedProperties++;
-      totalSharedPrice = totalSharedPrice + nItem["price"];
-    }
-  })
-
-  entireAverage = totalEntirePrice / numberOfEntireProperties;
-  privateAverage = totalPrivatePrice / numberOfPrivateProperties;
-  sharedAverage = totalSharedPrice / numberOfSharedProperties;
-
-  dAverages[key] = {
-    "totalNumProperties": sortedListings[key].length,
-    "entireAveragePrice": entireAverage,
-    "entireNumProperties": numberOfEntireProperties,
-    "entirePercentage": numberOfEntireProperties / sortedListings[key].length,
-    "privateAveragePrice": privateAverage,
-    "privateNumProperties": numberOfPrivateProperties,
-    "privatePercentage": numberOfPrivateProperties / sortedListings[key].length,
-    "sharedAveragePrice": sharedAverage,
-    "sharedNumProperties": numberOfSharedProperties,
-    "sharedPercentage": numberOfSharedProperties / sortedListings[key].length,
-    "propertiesComparison": [
-      {"propertyType":"Entire home/apt", "numberOfProperties":numberOfEntireProperties}, 
-      {"propertyType":"Private room", "numberOfProperties":numberOfPrivateProperties}, 
-      {"propertyType":"Shared room", "numberOfProperties":numberOfSharedProperties}
-    ]
-  };
-});
-
-mapData["features"].forEach(function(featuresItem){
-  var neighbourhood = featuresItem["properties"]["neighbourhood"];
-  featuresItem["properties"]["stats"] = dAverages[neighbourhood];
-})
-
-// console.log(JSON.stringify(mapData));
-
-module.exports = Averages;
+export default new Polygon(mapData, listingsData);
--- test/map-test.js
+++ test/map-test.js
@@ -1,9 +1,9 @@
-var chai = require('chai');
-var expect = chai.expect;
+const chai = require('chai');
+const expect = chai.expect;
 
-let Averages = require('../src/js/averages');
+import app from '../src/js/averages';
 
-let fixtureMap = 
+const fixtureMap = 
 {
     type: "FeatureCollection",
     features: [
@@ -41,15 +41,66 @@ let fixtureMap =
             }
         }
     ]
-}
+};
 
-describe("Borough List", function(){
+const fixtureListings =
+[
+  {
+    name: "Cozy, 3 minutes to Piccadilly Line",
+    neighbourhood: "Kingston upon Thames",
+    room_type: "Private room",
+    price: 35,
+  },
+  {
+    name: "The Sanctuary",
+    neighbourhood: "Kingston upon Thames",
+    room_type: "Private room",
+    price: 70,
+  },
+  {
+    name: "Holiday London DB Room Let-on going",
+    neighbourhood: "Croydon",
+    room_type: "Private room",
+    price: 65,
+  },
+  {
+    name: "Superb 3-Bed/2 Bath & Wifi: Trendy W1",
+    neighbourhood: "Kingston upon Thames",
+    room_type: "Entire home/apt",
+    price: 300,
+  },
+  {
+    name: "Battersea 2 bedroom house & parking",
+    neighbourhood: "Croydon",
+    room_type: "Entire home/apt",
+    price: 150,
+  },
+  {
+    name: "The Sanctuary",
+    neighbourhood: "Bromley",
+    room_type: "Private room",
+    price: 70,
+  }
+];
+
+const boroughList = ["Kingston upon Thames","Croydon","Bromley"];
+
+describe("createList()", function(){
     it("Creates a array of boroughs", function (){
-        
-        let arrayList = Averages.createBoroughList(fixtureMap);
-        let expectedArrayList = ["Kingston upon Thames","Croydon","Bromley"];
+        let createListResult = app.createList(fixtureMap);
+        expect(createListResult).is.eql(boroughList);
+    });
+});
 
-        expect(arrayList).is.eql(expectedArrayList);
+describe("sortListings()", function(){
 
+    let sortListingsResult = app.sortListings(fixtureListings, boroughList);
+
+    it("Populates the array with listings", function (){
+        expect(sortListingsResult).to.contain.keys('Bromley', 'Kingston upon Thames', 'Croydon');;
+    });
+
+    it("Other", function (){
+        expect(sortListingsResult["Kingston upon Thames"].length).to.equal(3);
     });
 });
