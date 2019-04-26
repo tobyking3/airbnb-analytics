d084be067e0048dce72ee2794aa0e132f74cb00f
Toby King <toby.king@bbc.co.uk>
2019-04-26 18:22:26 +0100

    initial scrolling functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './scss/styles.scss';
 import * as d3 from 'd3';
 import 'd3-selection-multi';
+import scroll from './js/scroll.js'
 import map from './js/map.js'
 import lineChart from './js/line-chart.js'
 import barChart from './js/bar-chart.js'
